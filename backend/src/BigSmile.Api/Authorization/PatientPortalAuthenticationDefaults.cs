d0b969b6ef7db433d8dc91732ffe7472400ca39d
romanrfhack <romanrfhack@gmail.com>
2026-07-25 15:57:20 -0600

    PI-2C2: define intake-only authorization policies

--- backend/src/BigSmile.Api/Authorization/PatientPortalAuthenticationDefaults.cs
+++ backend/src/BigSmile.Api/Authorization/PatientPortalAuthenticationDefaults.cs
@@ -5,6 +5,8 @@ namespace BigSmile.Api.Authorization
         public const string SelectorScheme = "BigSmileBearerSelector";
         public const string PatientBearerScheme = "PatientPortalBearer";
         public const string PatientSelfPolicy = "patientportal.self";
+        public const string IntakeOnlyPolicy = "patientportal.intake-only";
+        public const string PatientIntakeSelfPolicy = "patientportal.intake.self";
         public const string PatientPathPrefix = "/api/patient-portal";
     }
 }
