11e2451bfdf8f401ed8b89eeea6b5837f19b8742
romanrfhack <romanrfhack@gmail.com>
2026-07-25 15:11:14 -0600

    PI-2C1: add waiting-room audit actor types

---
+++ backend/src/BigSmile.Domain/Entities/PatientIntakeAccessLinkAuditActorType.cs
@@ -0,0 +1,9 @@
+namespace BigSmile.Domain.Entities
+{
+    public enum PatientIntakeAccessLinkAuditActorType
+    {
+        StaffUser = 1,
+        PatientPortalAccount = 2,
+        System = 3
+    }
+}
