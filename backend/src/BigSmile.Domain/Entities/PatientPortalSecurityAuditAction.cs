b99d4595402eaed7d52d860c0f8e3d2fa6067167
romanrfhack <romanrfhack@gmail.com>
2026-07-24 18:38:11 -0600

    feat: add patient portal invitation audit actions

---
+++ backend/src/BigSmile.Domain/Entities/PatientPortalSecurityAuditAction.cs
@@ -0,0 +1,9 @@
+namespace BigSmile.Domain.Entities
+{
+    public enum PatientPortalSecurityAuditAction
+    {
+        InvitationIssued = 1,
+        InvitationRevoked = 2,
+        InvitationSuperseded = 3
+    }
+}
